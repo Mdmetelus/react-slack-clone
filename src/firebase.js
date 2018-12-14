8a94dcefda755a4d858d3a1c2513ee08479c6c6a
Mr. Metelus <Mdmetelus@gmail.com>
2018-12-14 00:04:38 -0500

    "firebase setup"

---
+++ src/firebase.js
@@ -0,0 +1,15 @@
+import firebase from 'firebase/app';
+import "firebase/auth";
+import "firebase/database";
+import "firebase/storage";
+
+
+var config = {
+    apiKey: "AIzaSyB36h_PxD20PxTp6GOGkS9_h-gL0qKm1Yw",
+    authDomain: "react-slack-clone-50152.firebaseapp.com",
+    databaseURL: "https://react-slack-clone-50152.firebaseio.com",
+    projectId: "react-slack-clone-50152",
+    storageBucket: "react-slack-clone-50152.appspot.com",
+    messagingSenderId: "314296941673"
+  };
+  firebase.initializeApp(config);
